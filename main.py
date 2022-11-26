0b15ca9dfd22716737fdb6c8df48e51a56c3285f
specialized boy <41695878+HarshaSatyavardhan@users.noreply.github.com>
2022-11-26 08:47:12 +0000

    added streamlit functionality

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import requests
+import json
+
+st.header("DEEP DTA")
+form = st.form("prediction_form")
+drug = form.text_input("Enter drug")
+target = form.text_input("Enter target")
+submit_button = form.form_submit_button("Predict")
+
+if submit_button == True:
+    response = requests.get(f"https://d4-deepdta.herokuapp.com/prediction?drug={drug}&target={target}")
+    st.write("Binding Affinity",response)
